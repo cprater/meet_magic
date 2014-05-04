3a9be0dc83bca541e6603b48745833f8a90c20bf
Chris Prater <cprater23@gmail.com>
2014-05-03 20:36:26 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MeetMagic::Application.config.secret_key_base = 'ed7b47f37574e5c64b85917d52aefae32a2556a352c636f6077eb7a8777d882d93e2648197e45fdd0b4dfc4d687c0caefaf3fa21d55a8b94509759214c9dd823'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MeetMagic::Application.config.session_store :cookie_store, key: '_meet_magic_session'
