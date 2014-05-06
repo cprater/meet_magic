c3c773cc86164e97d5f9318e39589145c016c388
Chris Prater <cprater23@gmail.com>
2014-05-06 16:51:28 -0500

    styled user sign in/up

---
+++ app/assets/javascripts/navBar.js
@@ -0,0 +1,15 @@
+// $(function(){
+// 	$(document).on('click', '#log-in', function(e){
+// 		e.preventDefault();
+		
+// 		$.ajax({
+// 			type: 'GET',
+// 			url: '/users/sign_in',
+// 			success: function(serverResponse){
+// 				console.log(serverResponse);
+// 			}
+// 		});
+
+// 	});
+
+// });
