216841d386196b70a9e1519f92ee20d5b8d0da4d
Chris Prater <cprater23@gmail.com>
2014-05-09 17:47:14 -0500

    info boxes displaying on map when clicked

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,64 +1,69 @@
-$(function(){
-
+function initialize(){
 	var map;
 	var gmap;
-	
-});
 
-function initialize(){
-		$.ajax({
+	$.ajax({
 		url: '/get_current_user_coords',
 		method: 'GET',
 		success: function(serverResponse){
 			var lat = serverResponse.coords.lat;
 			var lng = serverResponse.coords.lng;
-			gmap = new GMap(lat,lng);
+			generateMap(lat,lng);
 			getAllUserCoords();
 		}
 	});
-}
 
-function getAllUserCoords(){
-	$.ajax({
-		url: '/get_all_user_coords',
-		method: 'GET',
-		success: function(serverResponse){
-			test = serverResponse.points;
-			placeAllPoints(serverResponse.points);
+	function generateMap(lat, lng){
+		var mapOptions = {
+			center: new google.maps.LatLng(lat, lng),
+			zoom: 8
+		};
+		map = new google.maps.Map(document.getElementById("map-canvas"),
+			mapOptions);
 		}
-	});
-}
 
-var placeMarker = function(info){
-	var marker = new google.maps.Marker({
-    position: new google.maps.LatLng(info.lat_lng.lat, info.lat_lng.lng),
-    title: info.name,
-    map: map
-  });
-};
-
-
-var placeAllPoints = function(points){
-	for(i = 0; i < points.length; i ++){
-		placeMarker(points[i]);
+	function placeMarker(info){
+		var marker = new google.maps.Marker({
+			position: new google.maps.LatLng(info.lat_lng.lat, info.lat_lng.lng),
+			title: info.name,
+			map: map,
+		});
+			addInfoWindow(marker, info);
 	}
-};
+
+	function placeAllPoints(points){
+		for(i = 0; i < points.length; i ++){
+			placeMarker(points[i]);
+		}
+	}
+
+	function getAllUserCoords(){
+		$.ajax({
+			url: '/get_all_user_coords',
+			method: 'GET',
+			success: function(serverResponse){
+				test = serverResponse.points;
+				placeAllPoints(serverResponse.points);
+			}
+		});
+	}
 
 
-var GMap = function(lat, lng){
-	var mapOptions = {
-    center: new google.maps.LatLng(lat, lng),
-    zoom: 8
-  };
-  map = new google.maps.Map(document.getElementById("map-canvas"),
-    mapOptions);
-};
+	function addInfoWindow(marker, info){
+		var infoBox = "<div class='info-box'>" +
+			"<h3 class='name'>" + info.name + "</h3>" +
+			"<ul class='info-window-stats'>" +
+				"<li> level: " + info.level + "</li>" +
+				"<li> score: " + info.score + "</li>" +
+			"</ul>" +
+		"</div>";
 
+		var infoWindow = new google.maps.InfoWindow({
+			content: infoBox
+		});
 
-
-
-
-
-
-
-
+		google.maps.event.addListener(marker, 'click', function() {
+			infoWindow.open(map,marker);
+		});
+	}
+}
