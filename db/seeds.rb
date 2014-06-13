9d6b4691d8a61d73afeb356cf50ad7d98471e317
Chris Prater <cprater23@gmail.com>
2014-06-13 12:49:17 -0600

    updated seed file for heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
 User.create(name: "admin", email: "c@p.com", password: "bigbootyjudy", password_confirmation: "bigbootyjudy", location: "Fort Worth, TX, United States", level: 100, score: 100, mentor: true)
 
-cities = ["Fort Worth, TX", "Denver, CO", "Chicago, IL"]
+cities = ["Fort Worth, TX", "Denver, CO", "Chicago, IL", "San Diego, CA", "New York, New York", "Atlanta, Georgia", "Orlando, FL", "Destin, Florida", "East Glacier, Montana", "Seattle, WA"]
 
-10.times do
+100.times do
 	password = Faker::Internet.password(8)
 	User.create(name: Faker::Name.first_name, email: Faker::Internet.email, password: password, password_confirmation: password, location: cities.sample, level: rand(3) + 1, score: Faker::Number.number(2), mentor: [true, false].sample )
 end
