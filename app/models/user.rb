a1a25e64d176fa0fcbcce9c6a125c450ec1c0fcb
Chris Prater <cprater23@gmail.com>
2014-06-13 13:24:35 -0600

    fixed user model to populate lat_lng appropriatel

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,22 +7,10 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
   
   geocoded_by :location
-  after_validation :geocode
-  before_save :populate_lat_lng, :populate_info
-
-  def populate_lat_lng
-  	self.lat_lng = find_lat_lng
-  end
+  after_validation :geocode, :populate_info
 
   def populate_info
-  	self.info = {"name" => self.name, "email" => self.email, "location" => self.location, "level" => self.level, "score" => self.score, "mentor" => self.mentor, "lat_lng" => self.lat_lng }
-  end
-
-private
-
-  def find_lat_lng
-    results = Geocoder.search(self.location)[0]
-    return results.geometry["location"] if results
+  	self.info = {"name" => self.name, "email" => self.email, "location" => self.location, "level" => self.level, "score" => self.score, "mentor" => self.mentor, "lat_lng" => {lat: self.latitude, lng: self.longitude} }
   end
   
 end
