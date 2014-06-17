2cbae4fca716b35a5013435cf905c40d06976c3c
Chris Prater <cprater23@gmail.com>
2014-06-17 14:53:10 -0600

    searches sort users by level descending

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,7 +2,7 @@ class SearchController < ApplicationController
 	def search
 		@input = capitalize_helper(params[:search][:input])
 		@users = User.find(:all, conditions: ['location LIKE ? OR name LIKE ?', "%#{@input}%", "%#{@input}%"])
-
+		@users.sort_by!{|u| -u.level}
 	end
 
 	
